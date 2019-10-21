4e4e296ab21c3192c6eb6fd7007ac2e59e1a6559
basic-app <dev@basic-app.com>
2019-10-21 11:09:13 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * @author Basic App Dev Team
- * @license MIT
- */
+
 $routes->add('user-provider', 'BasicApp\UserProvider\Controllers\UserProvider::index');
 $routes->add('user-provider/(:segment)', 'BasicApp\UserProvider\Controllers\UserProvider::$1');
 $routes->add('user-provider/(:segment)/(:segment)', 'BasicApp\UserProvider\Controllers\UserProvider::$1/$2');
